5a0e365445cc36e2b48d9b2572f7e7b220781411
Artemka314 <102158509+Artemka314@users.noreply.github.com>
2022-03-22 19:16:15 +0300

    ДЗ Третий урок

---
+++ src/Lesson3/Lesson3.java
@@ -0,0 +1,12 @@
+package Lesson3;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+
+        char i;
+        for(i=0; i<100; i++){
+            System.out.println(i);
+        }
+    }
+}
+
