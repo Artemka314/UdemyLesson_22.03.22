15d82534f8ff947f1fe0eb7381265d32aedec2e6
Artemka314 <102158509+Artemka314@users.noreply.github.com>
2022-03-28 10:05:52 +0300

    Доделал 6 урок

--- src/lesson6/lessn6.java
+++ src/lesson6/lessn6.java
@@ -1,7 +1,41 @@
 package lesson6;
+class lesson6Test{
 
+    int chisloq(int q,int w,int e,int r){
+
+int result = q+w+e+r;
+System.out.println(result);
+        return result;
+
+    }
+    int chisloq(int q,int w) {
+
+       int result = q + w;
+        System.out.println(result);
+        return result;
+    }
+
+    int chisloq(int q,int w, int e){
+
+        int result = q+w+e;
+        System.out.println(result);
+        return result;
+
+
+    }
+    int chisloq(){
+
+        int result = 0;
+        System.out.println(result);
+        return result;
+
+
+    }
+
+}
 public class lessn6 {
     public static void main(String[] args) {
-
+lesson6Test ls = new lesson6Test();
+ls.chisloq();
     }
 }
